2db352160389cc625a7022d27b922a1f63eb99d0
MateusAlvesBittencourt <94574370+MateusAlvesBittencourt@users.noreply.github.com>
2022-09-12 13:00:05 -0300

    Create 1015.java

---
+++ 1015.java
@@ -0,0 +1,18 @@
+//* Distancia entre dois Pontos */
+
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+        double distancia = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+        System.out.println(""+String.format("%.4f",distancia));
+        sc.close();
+    }
+}
