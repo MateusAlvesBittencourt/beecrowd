a31333ec575fc24118df5dcc78d2a3dc98371966
MateusAlvesBittencourt <94574370+MateusAlvesBittencourt@users.noreply.github.com>
2022-09-12 12:39:18 -0300

    Create 1003.java

---
+++ 1003.java
@@ -0,0 +1,19 @@
+//* Soma Simples*/
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+       Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+       sc.close();
+        int soma = a + b;
+        System.out.println("SOMA = " + soma);
+ 
+    }
+ 
+}
