f320186d39358bdcb8e1c89f903a2bc9ace30adf
MateusAlvesBittencourt <94574370+MateusAlvesBittencourt@users.noreply.github.com>
2022-09-12 13:06:47 -0300

    Create 1020.java

---
+++ 1020.java
@@ -0,0 +1,21 @@
+//* Idade em Dias */
+
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in); 
+        int idade = sc.nextInt();
+        int ano = idade / 365;
+        int mes = (idade % 365) / 30;
+        int dia = (idade % 365) % 30;
+
+        System.out.println(ano + " ano(s)");    
+        System.out.println(mes + " mes(es)");
+        System.out.println(dia + " dia(s)");
+
+        sc.close();
+    }
+}
